f1cd57bb86776b45942ad13517765ee7e24aa067
Michael <michal.aleksandrowicz@pw.edu.pl>
2023-11-11 01:57:48 +0000

    backward added

--- G06.R
+++ G06.R
@@ -11,7 +11,7 @@ netup <- function(d){
   W <- list()
   for(i in 1:(length(d)-1)){
     area <- d[i]* d[i+1]
-    W[[length(W) + 1]] <- matrix(runif(area,0,0.2), nrow = d[i],ncol = d[i+1])
+    W[[length(W) + 1]] <- matrix(runif(area,0,0.2), nrow = d[i+1],ncol = d[i])
   }
   
   #create b
@@ -24,19 +24,56 @@ netup <- function(d){
   return(network_list)
 }
 
+softmax <- function(vec_out){
+  copy_vec <- vec_out
+  
+  for(i in 1:length(copy_vec)){
+    copy_vec[i] <- exp(vec_out[i])/sum(exp(vec_out))
+  }
+  
+  return(copy_vec)
+}
+
 forward <- function(nn,inp){
   copy_network <- nn
   
   copy_network$h[[1]] <- inp
   for(i in 1:(length(copy_network$h)-1)){
-    copy_network$h[[i+1]] <- pmax(copy_network$W[[i]]%*%copy_network$h[[i]]+copy_network$b[[i]],0)
+    
+    #apply relu to hidden layers only
+    if(i!=(length(copy_network$h)-1)){
+      copy_network$h[[i+1]] <- pmax(copy_network$W[[i]]%*%copy_network$h[[i]]+copy_network$b[[i]],0)
+    }else{ #and softmax to the output layer
+      copy_network$h[[i+1]] <- softmax(copy_network$W[[i]]%*%copy_network$h[[i]]+copy_network$b[[i]])
+    }
+    
   }
   
   return(copy_network)
 }
 
 backward <- function(nn,k){
+  label_pos <- which(k==1)
   
+  #calculate the loss for the current inp vector
+  loss_deriv <- tail(nn$h,1)[[1]]-k
+  
+  dh <- list()
+  dh[[length(nn$W)+1]] <- loss_deriv
+  
+  dW <- list()
+  db <- list()
+  #backpropagate the loss
+  for(i in rev(seq(1:length(nn$W)))){
+    
+    dh[[i]] <- t(nn$W[[i]])%*%pmax(dh[[i+1]],0)
+    dW[[i]] <- pmax(dh[[i+1]],0)%*%t(nn$h[[i]])
+    db[[i]] <- pmax(dh[[i+1]],0)
+  }
+  
+  #add derivatives to the results list
+  derivatives <- list("dh" = dh, "dW" = dW, "db" = db)
+  return(c(nn, derivatives))
 }
 
 train <- function(nn,inp,k,eta=.01,mb=10,nstep=10000){
